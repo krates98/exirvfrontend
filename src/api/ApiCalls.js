72771adaf21758bc2ed52ca2d4b47e83e75799cc
krates98 <67364288+krates98@users.noreply.github.com>
2023-01-12 18:15:05 +0530

    fixing https

--- src/api/ApiCalls.js
+++ src/api/ApiCalls.js
@@ -6,7 +6,7 @@ const token = getToken();
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 const ipCall = axios.create({
-  baseURL: "http://ip-api.com/json/",
+  baseURL: "https://ip-api.com/json/",
 });
 
 // For common config
@@ -14,23 +14,23 @@ const ipCall = axios.create({
 axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
 const stateDataApi = axios.create({
-  baseURL: "http://exirvbackend.herokuapp.com/api/fetchdata/",
+  baseURL: "https://exirvbackend.herokuapp.com/api/fetchdata/",
 });
 
 const userIp = axios.create({
-  baseURL: "http://exirvbackend.herokuapp.com/api/userip/",
+  baseURL: "https://exirvbackend.herokuapp.com/api/userip/",
 });
 
 const adminApi = axios.create({
-  baseURL: "http://exirvbackend.herokuapp.com/api/admin/",
+  baseURL: "https://exirvbackend.herokuapp.com/api/admin/",
 });
 
 const attendanceApi = axios.create({
-  baseURL: "http://exirvbackend.herokuapp.com/api/user/",
+  baseURL: "https://exirvbackend.herokuapp.com/api/user/",
 });
 
 const advertiserApi = axios.create({
-  baseURL: "http://exirvbackend.herokuapp.com/api/advertiser/",
+  baseURL: "https://exirvbackend.herokuapp.com/api/advertiser/",
 });
 
 export { ipCall, stateDataApi, userIp, adminApi, advertiserApi, attendanceApi };
